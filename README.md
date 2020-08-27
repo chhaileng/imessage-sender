418ee9f3852d71c384424b918f3f5ac9f982b78a
Chhaileng Peng <chhailengpeng@gmail.com>
2020-08-27 22:44:48 +0900

    iMessages sender

---
+++ README.md
@@ -0,0 +1,13 @@
+# iMessage Sender Lol
+
+Sample script for sending iMessage using Python3 and Apple Script.
+
+Usage (MacOS with active iMessage only):
+
+```bash
+python3 main.py "phone/email"
+```
+
+> P.S. You can only use Apply Script to do this, but I used Python to make it simple xD
+
+Inspired by my girlfriend LOL.
---
+++ main.py
@@ -0,0 +1,23 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+
+# Author: Chhaileng Peng <contact@chhaileng.com>
+# GitHub: @chhaileng
+
+import sys
+import os
+import time
+
+if len(sys.argv) < 2:
+    print("""Usage: python3 main.py \"phone/email\"""")
+    print("""   Example: python3 main.py \"imsg@icloud.com\"""")
+    exit(5)
+
+to = sys.argv[1]
+
+#to = "phone/email" 
+msg = "I love you"
+
+for i in range(5):
+  print("Sending '" + msg + "' ...")
+  os.system('osascript scripts/sendiMessage.scpt "' + to + '" "' + msg + '"')
+  time.sleep(0.5)
